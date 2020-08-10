198d628357275d71162d282c932dffb2c3e292e4
Udeagbala Anthony <tonyudeagbala@gmail.com>
2020-08-10 15:11:00 +0000

    Created Database model in app.py. Database file test.db was created.

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # Databse Configuration And Location
+db = SQLAlchemy(app) # Initialize Database
+
+# Database Structure
+class Todo(db.Model):
+    # Database Columns
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__():
+        return '<Task %s>' % self.id
 
 @app.route("/")
 def index():
