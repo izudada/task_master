ad9334853d53dbd874d2d3f939c7e1c73ce002c2
Udeagbala Anthony <tonyudeagbala@gmail.com>
2020-08-11 10:12:01 +0000

    Insert a record in inex.html and modified the html structure. Record was inserted in test.db model. Styled the index.html in main.css.  Modified the index controller function.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # Databse Configuration And Location
 db = SQLAlchemy(app) # Initialize Database
+
 
 # Database Structure
 class Todo(db.Model):
@@ -17,9 +18,25 @@ class Todo(db.Model):
     def __repr__():
         return '<Task %s>' % self.id
 
-@app.route("/")
+
+@app.route("/", methods= ['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_name = request.form['task']
+        task_content = request.form['content']
+
+        new_task = Todo(task=task_name, content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Sorry, There was an issue creating your Task."
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
 
 
 if __name__ == "__main__":
